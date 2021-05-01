dc72da36accb03b0241dbf56ef358b8b88b05008
r0ck1n70sh <r0ck1n70sh@gmail.com>
2021-05-01 16:33:58 +0530

    prototype

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 $('#btn-gen').click(() => {
     $.ajax({
         method: 'GET',
-        url: 'https://theysaidso.com/img/qod/qod-inspire.jpg'
+        url: 'https://goquotes-api.herokuapp.com/api/v1/random?count=1'
     })
     .done((res) => {
         console.log(res);
+        $('.p-quote').text(res.quotes[0].text);
+        $('.p-auth').text(res.quotes[0].author);
+
+        console.log(res.quotes[0].text);
+        console.log(res.quotes[0].author)
     })
     .fail((error) => {
         console.error(error);
